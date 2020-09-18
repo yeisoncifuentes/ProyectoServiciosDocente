a87c623afc87a60d5f1dce5ce671b4d69ee211fd
Camilo Sanabria <cass465andres@gmail.com>
2020-09-17 23:03:23 -0500

    Documentación de servicios

--- ProyectoServiciosDocente/ProyectoServiciosDocente-web/src/main/java/co/edu/unicundi/services/DocenteService.java
+++ ProyectoServiciosDocente/ProyectoServiciosDocente-web/src/main/java/co/edu/unicundi/services/DocenteService.java
@@ -8,6 +8,9 @@ package co.edu.unicundi.services;
 import co.edu.unicundi.docentePOJO.DocentePOJO;
 import co.edu.unicundi.logica.LogicaDocente;
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.validation.Valid;
@@ -40,6 +43,15 @@ public class DocenteService {
      */
     @POST
     @Path("/registrar")
+    @ApiOperation(value = "Registra un docente en el sistema")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "El docente fue registrado correctamente, se devuelve el objeto creado"),
+        @ApiResponse(code = 400, message = " Error en la petición, los campos enviados en la solicitud son inválidos o el\n"
+                + "correo o la cédula ya están registrados o si la petición no contiene body"),
+        @ApiResponse(code = 404, message = "Recurso no encontrado"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es POST"),
+        @ApiResponse(code = 515, message = "El tipo de cuerpo en la petición no es Json"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response registrar(@Valid DocentePOJO docente) {
@@ -55,6 +67,14 @@ public class DocenteService {
     @Path("/listar")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Lista todos los docentes registrados")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Los docentes se obtuvieron correctamente"),
+        @ApiResponse(code = 204, message = "La lista de docentes está vacía"),
+        @ApiResponse(code = 400, message = "Error en la petición, puede suceder si se envía body"),
+        @ApiResponse(code = 404, message = "Recurso no encontrado"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es GET"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     public Response listar() {
         List<DocentePOJO> docentes = new LogicaDocente().listar();
         return Response.status(Response.Status.OK).entity(docentes).build();
@@ -69,6 +89,13 @@ public class DocenteService {
     @Path("/obtenerPorCedula/{cedula}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Obtiene un docente registrado filtrando por cedula")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "El docente filtrado se obtuvo correctamente"),
+        @ApiResponse(code = 400, message = "Error en la petición, puede suceder si se envía body"),
+        @ApiResponse(code = 404, message = "Recurso o docente no encontrado"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es GET"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     public Response obtenerPorCedula(@PathParam("cedula") String cedula) throws Exception {
         DocentePOJO docente = new LogicaDocente().obtenerPorCedula(cedula);
         return Response.status(Response.Status.OK).entity(docente).build();
@@ -83,6 +110,13 @@ public class DocenteService {
     @Path("/obtenerDocentesMateria/{materia}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Lista todos los docentes filtrando por materia")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Los docentes se obtienen correctamente"),
+        @ApiResponse(code = 400, message = "Error en la petición, puede suceder si se envía body"),
+        @ApiResponse(code = 404, message = "Recurso encontrado o lista vacia"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es GET"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     public Response obtenerDocentesMateria(@PathParam("materia") String materia) throws Exception {
         List<DocentePOJO> docentes = new LogicaDocente().obtenerDocentesMateria(materia);
         return Response.status(Response.Status.OK).entity(docentes).build();
@@ -98,6 +132,15 @@ public class DocenteService {
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Edita los datos de un docente en el sistema")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Se editaron los datos correctamente"),
+        @ApiResponse(code = 400, message = " Error en la petición, los campos enviados en la solicitud son inválidos o el\n"
+                + "correo o la cédula ya están registrados o si la petición no contiene body"),
+        @ApiResponse(code = 404, message = "Recurso o docente no encontrado"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es PUT"),
+        @ApiResponse(code = 515, message = "El tipo de cuerpo en la petición no es Json"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     public Response editar(@Valid DocentePOJO docente) {
         new LogicaDocente().editar(docente);
         return Response.status(Response.Status.OK).entity("Editado correctamente").build();
@@ -112,6 +155,13 @@ public class DocenteService {
     @Path("/eliminar/{id}")
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Elimina un docente registrado con el id especificado")
+    @ApiResponses(value = {
+        @ApiResponse(code = 204, message = "El docente se eliminó correctamente"),
+        @ApiResponse(code = 400, message = "Error en la petición, puede suceder sí se envía body"),
+        @ApiResponse(code = 404, message = "Recurso o docente no encontrado"),
+        @ApiResponse(code = 405, message = "El método de la solicitud no es DELETE"),
+        @ApiResponse(code = 500, message = "Error en el servidor o base de datos")})
     public Response eliminar(@PathParam("id") int id) {
         new LogicaDocente().eliminar(id);
         return Response.status(Response.Status.NO_CONTENT).build();
