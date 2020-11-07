bf47350bdbe216cd47c4b3927757786ad4b1da82
Camilo Sanabria <cass465andres@gmail.com>
2020-11-07 09:59:29 -0500

    AbstractFacade

---
+++ ProyectoServiciosDocente/ProyectoServiciosDocente-ejb/src/main/java/co/edu/unicundi/repo/AbstractFacade.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.unicundi.repo;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author cass465
+ */
+public abstract class AbstractFacade<T, V> {
+
+    private Class<T> entityClass;
+    
+    public AbstractFacade(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+    
+    protected abstract EntityManager getEntityManager();
+    
+    public void registrar(T entity) {
+        getEntityManager().persist(entity);
+    }
+
+    public List<T> listar() {
+        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
+    }
+
+    public void editar(T entity) {
+        getEntityManager().merge(entity);
+    }
+
+    public void eliminar(T entity) {
+        getEntityManager().remove(entity);
+    }
+
+    public T obtenerPorId(V id) {
+        return getEntityManager().find(entityClass, id);
+    }
+}
