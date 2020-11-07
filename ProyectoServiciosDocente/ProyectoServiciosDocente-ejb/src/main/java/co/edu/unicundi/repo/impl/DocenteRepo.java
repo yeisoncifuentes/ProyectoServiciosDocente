cb5cffa49e23d78ca096b8681517ba32dac8af99
Camilo Sanabria <cass465andres@gmail.com>
2020-11-07 09:59:56 -0500

    AbstractFacade en DocenteRepo

--- ProyectoServiciosDocente/ProyectoServiciosDocente-ejb/src/main/java/co/edu/unicundi/repo/impl/DocenteRepo.java
+++ ProyectoServiciosDocente/ProyectoServiciosDocente-ejb/src/main/java/co/edu/unicundi/repo/impl/DocenteRepo.java
@@ -6,6 +6,7 @@
 package co.edu.unicundi.repo.impl;
 
 import co.edu.unicundi.entity.Docente;
+import co.edu.unicundi.repo.AbstractFacade;
 import co.edu.unicundi.repo.IDocenteRepo;
 import java.util.ArrayList;
 import java.util.Date;
@@ -24,20 +25,18 @@ import javax.persistence.TypedQuery;
  * @author cass465
  */
 @Stateless
-public class DocenteRepo implements IDocenteRepo {
+public class DocenteRepo extends AbstractFacade<Docente, Integer> implements IDocenteRepo {
 
     @PersistenceContext(unitName = "edu.unicundi_ProyectoServiciosDocente-ejb_ejb_1.0-SNAPSHOTPU")
     private EntityManager entity;
 
-    @Override
-    public void registrar(Docente docente) {
-        entity.persist(docente);
+    public DocenteRepo() {
+        super(Docente.class);
     }
-
+    
     @Override
-    public List<Docente> listar() {
-        TypedQuery<Docente> query = this.entity.createQuery("SELECT d FROM Docente d", Docente.class);
-        return query.getResultList();
+    protected EntityManager getEntityManager() {
+        return entity;
     }
 
     @Override
@@ -77,21 +76,6 @@ public class DocenteRepo implements IDocenteRepo {
     }
 
     @Override
-    public void editar(Docente docente) {
-        entity.merge(docente);
-    }
-
-    @Override
-    public void eliminar(Docente docente) {
-        entity.remove(docente);
-    }
-
-    @Override
-    public Docente obtenerPorId(Integer id) {
-        return entity.find(Docente.class, id);
-    }
-
-    @Override
     public Docente obtenerPorCedula(String cedula) {
         try {
             Query query = entity.createQuery("SELECT d FROM Docente d WHERE d.cedula = ?1", Docente.class);
@@ -119,5 +103,4 @@ public class DocenteRepo implements IDocenteRepo {
     public List<Docente> listarPorMateria(String materia) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
 }
